c339ab20dbcf124433514cd7ac61a917bdf7f5d2
DotNet Ninja <chaos.monkey@hotmail.com>
2023-01-08 13:21:28 -0600

    Added Redirect Entity

---
+++ src/DotNetNinja.QrCodes/Entities/Redirect.cs
@@ -0,0 +1,11 @@
+﻿using DotNetNinja.QrCodes.Constants;
+
+namespace DotNetNinja.QrCodes.Entities;
+
+public class Redirect
+{
+    public string Id { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Uri Url { get; set; } = Default.RedirectUri;
+    public bool IsEnabled { get; set; } = false;
+}
